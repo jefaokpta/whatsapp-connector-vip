90c6b4ebfea9cf3fa93f8a52907e896fd77e8400
jefaokpta <jefaokpta@hotmail.com>
2022-01-13 15:42:07 -0300

    Dockerfile em prod

--- src/static/staticVar.ts
+++ src/static/staticVar.ts
@@ -4,6 +4,6 @@
 
 // DOCKER
 export const mediaFolder = '/whatsMedia'
-export const urlBase = 'http://192.168.15.152:8080' // LOCAL
+// export const urlBase = 'http://192.168.15.152:8080' // LOCAL
 // export const urlBase = 'http://128.0.0.1:9090' // HOMOLOGATION
-// export const urlBase = 'http://128.0.0.1:8080' // PRODUCTION
+export const urlBase = 'http://128.0.0.1:8080' // PRODUCTION
